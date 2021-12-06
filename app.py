7f761ce431ed7f3016e002aafda3b17d0db21b44
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2021-12-05 19:14:01 -0500

    Basic requirements complete!
    Done for the class now!

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from enum import unique
+from typing import Reversible
 from flask import Flask, app, render_template, url_for, redirect
 import flask
 from flask.helpers import flash
+from flask_login.utils import login_fresh
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_manager, login_user, LoginManager, login_required,logout_user, current_user
 from flask_wtf.form import FlaskForm
@@ -38,7 +40,19 @@ class User(db.Model, UserMixin): # creating the table
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
 
+class BookStore(db.Model, UserMixin): # creating the table
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False, unique=False)
+    description = db.Column(db.String(200), nullable=False, unique=False)
+    url = db.Column(db.String(80), nullable=False, unique=False)
+    #material_id = db.Column(db.String(80), nullable=False)
 
+class ReadList(db.Model, UserMixin): # creating the table
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False, unique=False)
+    book_id = db.Column(db.Integer, nullable=False, unique=False)
+
+# register and log in forms
 class RegisterForm(FlaskForm):
     firstname = StringField(validators=[InputRequired(), Length(min=4,max=20)],
     render_kw={"placeholder": "First name"})
@@ -75,6 +89,22 @@ class LoginForm(FlaskForm):
 @app.route('/')
 def index():
     """render index html"""
+    """# add to table BookStore
+    add_material = BookStore(title='The davinci code', description="The davinci code bla bla", url='https://www.bookstellyouwhy.com/pictures/53669.jpeg?v=1552765526 ')
+    #db.create_all() # create the table
+    db.session.add(add_material)
+    db.session.commit()
+    add_material = BookStore(title='Beloved', description="Beloved ba ba ba", url='https://upload.wikimedia.org/wikipedia/commons/6/6f/Beloved_%281987_1st_ed_dust_jacket_cover%29.jpg')
+    #db.create_all() # create the table
+    db.session.add(add_material)
+    db.session.commit()
+    add_material = BookStore(title='We are each others harvest', description="We are bla bla", url='https://epmgaa.media.clients.ellingtoncms.com/img/croppedphotos/2021/04/13/booksWe_Are_Each_Others_Harvest_t750x550.jpg?d885fc46c41745b3b5de550c70336c1b382931d2')
+    #db.create_all() # create the table
+    db.session.add(add_material)
+    db.session.commit()"""
+
+    logout_user() # log out user if any on the session
+    print('done')
     print(current_user.is_authenticated)
     return render_template('index.html', auth=current_user.is_authenticated)
 
@@ -133,11 +163,68 @@ def logout():
 def dashboard():
     # object username
     #print(load_user(user_id_print).username) -- gives me the id and username of logged person
-    #market = SampleMarket.query.all()
+    books = BookStore.query.all()
+    #print(books)
     user = User.query.filter_by(id=global_user_id).first()
-    flash(f'Welcome back {user.firstname} {user.lastname}!!!')
+    # remove the flash later
+    #flash(f'Welcome back {user.firstname} {user.lastname}!!!')
     #print(user.firstname)
-    return render_template('dashboard.html', username=global_username)
+    return render_template('dashboard.html', username=global_username, books=books, firstname=user.firstname, lastname=user.lastname )
+
+@app.route('/myreadinglist',methods=['GET', 'POST'])
+@login_required
+def myreadinglist():
+    # object username
+    #print(load_user(user_id_print).username) -- gives me the id and username of logged person
+    #books = BookStore.query.all()
+    #print(books)
+    #user = User.query.filter_by(id=global_user_id).first()
+    # remove the flash later
+    #flash(f'Welcome back {user.firstname} {user.lastname}!!!')
+    #print(user.firstname)
+    readlist_object = ReadList.query.filter_by(user_id=global_user_id).all()
+    #print(cart_list)
+    books_list = []
+    #print(readlist_object)
+    for books in readlist_object:
+    #    #print(material.material_id)
+        books_list.append(BookStore.query.filter_by(id=books.book_id).first())
+
+    return render_template('myreadinglist.html', username=global_username, books_list=books_list)
+
+
+
+@app.route('/<int:id>/addtoreadinglist', methods=('POST',))
+def add_to_readingList(id):
+    #print(id, 'book id')
+    #print('something gioing on here')
+    #print(user_id_print, "user id")
+    
+    book_object = ReadList.query.filter_by(user_id=global_user_id).all()
+    print(book_object, "book")
+    book_ids = list(map(lambda x: (x.book_id), book_object))
+    if id not in book_ids: # check if it already existes
+        print("adding material")
+        add_material_to_cart = ReadList(user_id=global_user_id, book_id=id)
+        db.create_all() # create the table
+        db.session.add(add_material_to_cart)
+        db.session.commit()
+        print("added sucessfully!")
+        flash('Successfully added to cart!')
+    else:
+        flash('The item is already in the cart!')
+    return redirect(url_for('dashboard'))
+
+@app.route('/<int:id>/remove', methods=('POST',))
+def remove_from_readinglist(id):
+    print(id, "id to remove")
+    ReadList.query.filter(ReadList.user_id == global_user_id, ReadList.book_id == id).delete()
+    db.session.commit()
+    
+    """for i in temp:
+        print(i.user_id)"""
+    return redirect(url_for('myreadinglist'))
+
 
 if __name__ == '__main__':
     db.create_all()
